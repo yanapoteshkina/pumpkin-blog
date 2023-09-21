7da3125afa767b4c564c29f02bf343aeb7d2dddf
yanap <tatiyanavoron19@gmail.com>
2023-09-21 18:37:14 +0600

    Добавление функциональности тегирования

--- pumpkin/blog/models.py
+++ pumpkin/blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from taggit.managers import TaggableManager
+
 
 
 class PublishedManager(models.Manager):
@@ -12,6 +14,8 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
+
+    tags = TaggableManager()
 
 
     class Status(models.TextChoices):
--- pumpkin/blog/views.py
+++ pumpkin/blog/views.py
@@ -10,6 +10,31 @@ from django.core.mail import send_mail
 
 from django.views.decorators.http import require_POST
 
+from taggit.models import Tag
+
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                 'blog/post/list.html',
+                 {'posts': posts,
+                  'tag': tag})
+
 #функция отправки коментария
 
 @require_POST
@@ -17,8 +42,6 @@ def post_comment(request, post_id):
     post = get_object_or_404(Post,
                     id=post_id,
                     status=Post.Status.PUBLISHED)
-    
-    
     
     comment = None
     # Комментарий был отправлен
