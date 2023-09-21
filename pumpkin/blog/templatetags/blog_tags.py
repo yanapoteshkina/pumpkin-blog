f5f2fc7c16313e16ab61e4672fcfeec9e1f83476
yanap <tatiyanavoron19@gmail.com>
2023-09-21 19:21:00 +0600

    Добавили markdownЭ

--- pumpkin/blog/templatetags/blog_tags.py
+++ pumpkin/blog/templatetags/blog_tags.py
@@ -2,8 +2,15 @@ from django import template
 from ..models import Post
 from django.db.models import Count
 
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
+
+@register.filter(name='markdown')
+def markdown_format(text):
+ return mark_safe(markdown.markdown(text))
+
 
 @register.simple_tag
 def total_posts():
